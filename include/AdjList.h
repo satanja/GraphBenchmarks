10e4c25c688c1e9513fa9d3e7ac785365d37d09c
satanja <s.a.tanja@student.tue.nl>
2020-07-23 15:53:07 +0200

    Add Adjacency List

---
+++ include/AdjList.h
@@ -0,0 +1,16 @@
+#include "Graph.h"
+
+#pragma once
+
+class AdjList : public Graph
+{
+public:
+    AdjList(size_t size);
+    void erase(int vertex);
+    void insert_edge(int source, int dest);
+    void resize(size_t vertices);
+    int vertices();
+private:
+    std::vector<std::vector<int>> adj;
+    int n;
+};
---
+++ src/AdjList.cpp
@@ -0,0 +1,33 @@
+#include "AdjList.h"
+
+AdjList::AdjList(size_t size)
+{
+    n = size;
+    adj.resize(n);
+}
+
+void AdjList::erase(int vertex)
+{
+    for (int dest : adj[vertex])
+    {
+       auto loc = std::find(adj[dest].begin(), adj[dest].end(), vertex);
+       adj[dest].erase(loc);
+    }
+    adj[vertex].clear();
+}
+
+void AdjList::insert_edge(int source, int dest)
+{
+    adj[source].push_back(dest);
+    adj[dest].push_back(source);
+}
+
+void AdjList::resize(size_t vertices)
+{
+    adj.resize(vertices, std::vector<int>(0));
+}
+
+int AdjList::vertices()
+{
+    return n;
+}
