0d5aa8cc717f346b23adf018b15f3420239d801a
satanja <s.a.tanja@student.tue.nl>
2020-07-25 14:10:40 +0200

    Add semi-arbitrary vertex deletion

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,30 @@
 
 #include <iostream>
 
+#include <cstdlib>
+
+void buildDeleteOrder(int vertices, std::vector<int>& del_order)
+{
+    std::srand(1); // fixed seed
+    std::vector<int> queue;
+    queue.resize(vertices);
+    del_order.resize(vertices);
+    for (int i = 0; i < vertices; i++)
+    {
+        queue[i] = i;
+    }
+    
+    int i = 0;
+    while (!queue.empty())
+    {
+        int index = std::rand() % queue.size();
+        int vertex = *(queue.begin() + index);
+        queue.erase(queue.begin() + index);
+        del_order[i] = vertex;
+        i++;
+    }
+}
+
 void buildGraph(std::shared_ptr<Graph> g)
 {
     std::string word;
@@ -31,9 +55,12 @@ void buildGraph(std::shared_ptr<Graph> g)
     }
 }
 
-void reduceGraph(std::shared_ptr<Graph> g)
+void reduceGraph(std::shared_ptr<Graph> g, std::vector<int> del_order)
 {
-
+    for (int i = 0; i < del_order.size(); i++)
+    {
+        g->erase(del_order[i]);
+    }
 }
 
 void traverseGraph(std::shared_ptr<Graph> g)
@@ -43,17 +70,23 @@ void traverseGraph(std::shared_ptr<Graph> g)
 
 int main()
 {   
-    auto g1 = std::make_shared<AdjHash>();
+    auto build_start = std::chrono::high_resolution_clock::now();
+    auto g1 = std::make_shared<AdjMatrix>();
     buildGraph(g1);
+    auto build_end = std::chrono::high_resolution_clock::now();
     
-    auto g2 = std::make_shared<AdjList>();
-    buildGraph(g2);
+    std::vector<int> del_order;
+    buildDeleteOrder(g1->vertices(), del_order);
 
-    auto g3 = std::make_shared<AdjMatrix>();
-    buildGraph(g3);
+    auto del_start = std::chrono::high_resolution_clock::now();
+    reduceGraph(g1, del_order);
+    auto del_end = std::chrono::high_resolution_clock::now();
 
-    auto g4 = std::make_shared<AdjSet>();
-    buildGraph(g4);
+    std::chrono::duration<double> build_diff = build_end - build_start;
+    std::chrono::duration<double> del_diff = del_end - del_start;
+    
+    std::cout << "building graph: " << build_diff.count() << " s\n";
+    std::cout << "deleting graph: " << del_diff.count() << " s\n";
 
     return 0;
 }
