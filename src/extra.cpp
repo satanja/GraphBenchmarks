226169f41de41dec551295127a4657cfc3edfa52
satanja <s.a.tanja@student.tue.nl>
2020-10-08 12:39:24 +0200

    Add Final Benches

--- src/extra.cpp
+++ src/extra.cpp
@@ -14,36 +14,29 @@ int main()
     output.open("minibench.csv");
     output << "length,deleting,Class\n";
 
-    for (int i = 2; i < 200000; i += 2)
+    for (int i = 2; i < 4000; i += 2)
     {
         std::vector<int> vec(i, 42);
-        vec[i / 2] = 1;
+        vec[i / 2] = i / 2;
         
         std::set<int> set;
         for (int j = 0; j < vec.size(); j++)
         {
-            if (j == i / 2) 
-            {
-                set.insert(1);
-            }
-            else 
-            {
-                set.insert(42);
-            }
+           set.insert(j);
         }
 
         // bench
         auto vec_start = std::chrono::high_resolution_clock::now();
-        auto ptr = std::find(vec.begin(), vec.end(), 1);
+        auto ptr = std::find(vec.begin(), vec.end(), i / 2);
         vec.erase(ptr);
         auto vec_end = std::chrono::high_resolution_clock::now();
 
         auto set_start = std::chrono::high_resolution_clock::now();
-        set.erase(1);
+        set.erase(i / 2);
         auto set_end = std::chrono::high_resolution_clock::now();
 
-        std::chrono::duration<double, std::milli> vec_diff = vec_end - vec_start;
-        std::chrono::duration<double, std::milli> set_diff = set_end - set_start;
+        std::chrono::duration<double, std::nano> vec_diff = vec_end - vec_start;
+        std::chrono::duration<double, std::nano> set_diff = set_end - set_start;
 
         // std::cout << vec_diff.count() << " " << set_diff.count() << std::endl;
         output << i << "," << vec_diff.count() << ",Vector\n";
